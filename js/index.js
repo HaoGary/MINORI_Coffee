2a115774a54b2941593f9da3fcde30b24851b701
HaoGary <chen.hao.tsung@gmail.com>
2021-12-17 16:19:08 +0800

    1217 UPDATE

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    // menu
+    // header menu
     const navSlide = function () {
         const burger_btn = document.querySelector('.humburger');
         const menu = document.querySelector('.menu');
@@ -9,5 +9,35 @@ window.onload = function () {
         });
     }
     navSlide();
+
+    // ==================== search btn
+    const btn = document.querySelector('.search-btn');
+    const input = document.querySelector('.search-input');
+
+    btn.addEventListener('click', function () {
+        input.classList.toggle('search-active');
+        input.focus();
+        // 如果有active 文字就顯示，不然就消失
+        if ($('.search-input').hasClass('search-active')) {
+            $('.search-input').css('opacity', '1');
+        } else {
+            $('.search-input').css('opacity', '0');
+        }
+    });
+    // ==================== user btn
+    const userBtn = document.querySelector(".user-btn");
+    const signMenu = document.querySelector(".signOut-menu");
+
+    userBtn.addEventListener('click', function () {
+        signMenu.classList.toggle('signOut-dropdown-active');
+        // if ($('.search-input').hasClass('search-active')) {
+        //     $('.search-input').css('opacity', '1');
+        // } else {
+        //     $('.search-input').css('opacity', '0');
+        // }
+    });
+
+
+
 
 };
